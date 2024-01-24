1b0ca671377b9c33c9d1a7cfd31e2f54e6a985b2
Leon Svensson <ltksvensson@gmail.com>
2024-01-24 13:25:04 +0100

    guh

--- extPage/scripts.js
+++ extPage/scripts.js
@@ -1,40 +1,48 @@
-const burgermenu_list = document.getElementById("burgermenu-listId");
-const burgermenu = document.getElementById("burgermenuId");
+const dropdown_list = document.getElementById("dropdown-listId");
+const dropdown = document.getElementById("dropdownId");
 const container = document.getElementById("passItem")
+
+const savePasswordButton = document.getElementById("savePasswordButton");
+
 import { ImpKey } from "../addPassword_script.js";
-var burgerMenuState = false; 
+var dropdownState = false; 
 
 window.onload = (event) => {
-    let Data = JSON.parse(localStorage.getItem(ImpKey))
+    console.log("HEJ")
+    let Data = [];
+    Data.push  
+    // let Data = JSON.parse(localStorage.getItem(ImpKey))
     console.log(Data);
-    //for (let i = 0; i < 3; i++){
-    //let ItemWebsiteName = Data.
-    //}
+    for (let i = 0; i < 3; i++){
+    let ItemWebsiteName = Data[0]
+    }
+    console.log("HEJ")
+    console.log(savePasswordButton)
 };
 
 container.addEventListener(
     "mouseover",
     (event) => {
-        burgermenu.style.display = "inline-flex"
+        dropdown.style.display = "inline-flex"
     },
     false,
 );
 container.addEventListener(
     "mouseleave",
     (event) => {
-        if (burgerMenuState == false){
-            burgermenu.style.display = "none";
+        if (dropdownState == false){
+            dropdown.style.display = "none";
         }
     },
     false,
 );
-burgermenu.addEventListener(
+dropdown.addEventListener(
     "mousedown",
     (event) => {
-        burgerMenuState ? 
-            burgermenu_list.style.display = "none" : 
-            burgermenu_list.style.display = "block";
-        burgerMenuState = !burgerMenuState
+        dropdownState ? 
+            dropdown_list.style.display = "none" : 
+            dropdown_list.style.display = "block";
+        dropdownState = !dropdownState
     },
     false,
 );
