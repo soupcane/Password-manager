7a4059d6bb61be67ce4223750aebd538d05b30a9
soupcane <50845658+soupcane@users.noreply.github.com>
2024-02-28 13:02:18 +0100

    fix

--- addPassword_script.js
+++ addPassword_script.js
@@ -1,69 +1,61 @@
-import {keyList} from './keys.js';
+(function () { // undviker globala variabler
+  const globalKey = 'asdf';
+  let keys = localStorage.getItem(globalKey);
+  let recieveKey;
+  window.onload = () => {
+    recieveKey = window.location.search;
+    recieveKey = recieveKey.replace('?', '');
 
-console.log(keyList.keys);
-let recieveKey;
-window.onload = () => {
-  recieveKey = window.location.search;
-  recieveKey = recieveKey.replace('?', '');
+    if (recieveKey != null && recieveKey !== '') // if there is no key then this is a new item
+    {
+      loginInformation = JSON.parse(this.localStorage.getItem(recieveKey));
 
-  if (recieveKey != null && recieveKey !== '') // if there is no key then this is a new item
-  {
-    loginInformation = JSON.parse(this.localStorage.getItem(recieveKey));
-
-    this.document.getElementById('username').value = loginInformation.username;
-    this.document.getElementById('password').value = loginInformation.password;
-    this.document.getElementById('siteLink').value = loginInformation.siteLink;
-  }
-}
-
-const savePasswordButton = document.getElementById('savePasswordButton');
-
-savePasswordButton.addEventListener('click', () => {
-  let loginInformation = {
-    username: document.getElementById('username').value,
-    password: document.getElementById('password').value,
-    siteLink: document.getElementById('siteLink').value,
+      this.document.getElementById('username').value = loginInformation.username;
+      this.document.getElementById('password').value = loginInformation.password;
+      this.document.getElementById('siteLink').value = loginInformation.siteLink;
+    }
   };
 
-  const loginInformationSerialized = JSON.stringify(loginInformation);
-  console.log(recieveKey);
-  if (recieveKey != null && recieveKey !== '') // if key is not null (meaning the item already exist) then write over last item with new edited item
-  {
-    localStorage.setItem(recieveKey, loginInformationSerialized);
+  const savePasswordButton = document.getElementById('savePasswordButton');
+
+  savePasswordButton.addEventListener('click', () => {
+    let loginInformation = {
+      username: document.getElementById('username').value,
+      password: document.getElementById('password').value,
+      siteLink: document.getElementById('siteLink').value,
+    };
+
+    const loginInformationSerialized = JSON.stringify(loginInformation);
+    console.log(recieveKey);
+    if (recieveKey != null && recieveKey !== '') {
+      localStorage.setItem(recieveKey, loginInformationSerialized);
+    } 
+    else { // Item doesn't exist
+      let tempkey = [];
+      tempkey.push(keyGenerator()); // Generates key for new item
+      localStorage.setItem(globalKey, tempkey);
+      console.log(`GENERATED KEY: ${tempkey}`);
+      localStorage.setItem(tempkey, loginInformationSerialized);
+      console.log(keys);
+    }
+  });
+
+  function keyGenerator() {
+    let chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let keyLength = 12;
+    let generatedKey = '';
+
+    for (let i = 0; i <= keyLength; i += 1) {
+      let randomNumber = Math.floor(Math.random() * chars.length);
+      generatedKey += chars.substring(randomNumber, randomNumber + 1);
+    }
+
+    return generatedKey;
   }
-  else { // Item doesn't exist
-    let tempkey = keyGenerator(); // Generates key for new item
-    keyList.keys.push(tempkey); 
-    console.log(`GENERATED KEY: ${tempkey}`);
-    localStorage.setItem(tempkey, loginInformationSerialized);
-    console.log(keyList.keys);
-  }
-});
 
-function showPassword() {
-  let x = document.getElementById('password');
-  if (x.type === 'password') {
-    x.type = 'text';
-  } else {
-    x.type = 'password';
-  }
-}
+  const clearLocalStorageButton = document.getElementById('clearLocalStorageButton');
 
-function keyGenerator() {
-  let chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
-  let keyLength = 12;
-  let generatedKey = '';
-
-  for (let i = 0; i <= keyLength; i += 1) {
-    let randomNumber = Math.floor(Math.random() * chars.length);
-    generatedKey += chars.substring(randomNumber, randomNumber + 1);
-  }
-
-  return generatedKey;
-}
-
-const clearLocalStorageButton = document.getElementById('clearLocalStorageButton')
-
-clearLocalStorageButton.addEventListener('click', () => {
-  localStorage.clear();
-});
+  clearLocalStorageButton.addEventListener('click', () => {
+    localStorage.clear();
+  });
+}());
--- extPage/scripts.js
+++ extPage/scripts.js
@@ -1,94 +1,103 @@
-import {keyList} from '../keys.js';
-console.log(keyList.keys);
-const template = document.querySelector('#templateId');
-let myTemplateClone = template.content.cloneNode(true); // clone the template
-const myUl = document.getElementById('uList');
+(function () { // undviker globala variabler
+  const globalKey = 'asdf';
+  const template = document.querySelector('#templateId');
+  let myTemplateClone = template.content.cloneNode(true); // clone the template
+  const myUl = document.getElementById('uList');
 
-let dropdownState = false;
+  let dropdownState = false;
 
-let loginInformation = {
-  username: '',
-  password: '',
-  siteLink: '',
-};
+  let loginInformation = {
+    username: '',
+    password: '',
+    siteLink: '',
+  };
 
-const passwordDataList = [];
-let elementId;
-
-keyList.keys.forEach((element) => {
-  loginInformation = JSON.parse(localStorage.getItem(element));
-  console.log(element);
-  if (loginInformation !== null){
-    const passwordData = { WEBSITE: loginInformation.siteLink, EMAIL: loginInformation.username };
-    passwordDataList.push(passwordData);
-    elementId += 1;
-  } else {
-    console.warn('no password data');
-  }
-});
-
-const addButton = document.querySelector('.addItem')
-addButton.addEventListener('click', event =>{
-  chrome.tabs.create({ url: chrome.runtime.getURL(`./addPassword.html`) });
-});
-
-window.onload = () => {
-  let itemId = 0;
-
-  passwordDataList.forEach((element) => {
-    myTemplateClone = template.content.cloneNode(true);
-
-    const nameId = myTemplateClone.getElementById('WebsiteNameId');
-    const emailId = myTemplateClone.getElementById('websiteEmailId');
-    const listItem = myTemplateClone.getElementById('listItem');
-
-    listItem.dataset.index = itemId.toString();
-    itemId += 1;
-    nameId.innerHTML = element.WEBSITE;
-    emailId.innerHTML = element.EMAIL;
-
-    myUl.appendChild(myTemplateClone);
-  });
-
-  const list = document.querySelectorAll('#listItem');
-  list.forEach((element) => {
-    const dropdown = element.querySelector('#dropdown');
-    const dropdownList = element.querySelector('#dropdown-list');
-    const itemContainer = element.querySelector('#itemContainer');
-    const editButton = element.querySelector('#editButton');
-
-    console.log(element);
-    itemContainer.addEventListener(
-      'mouseover',
-      () => {
-        dropdown.style.display = 'inline-flex';
-      },
-      false,
-    );
-
-    itemContainer.addEventListener(
-      'mouseleave',
-      () => {
-        if (dropdownState === false) {
-          dropdown.style.display = 'none';
-        }
-      },
-      false,
-    );
-
-    dropdown.addEventListener(
-      'mousedown',
-      () => {
-        dropdownState ? dropdownList.style.display = 'none' : dropdownList.style.display = 'block';
-        dropdownState = !dropdownState;
-      },
-      false,
-    );
-    console.log('first')
-    editButton.addEventListener('click', event =>{
-      console.log('second');
-      window.location.href = `../addPassword.html?${keyList.keys[element.dataset.index]}`;
-      chrome.tabs.create({ url: chrome.runtime.getURL(`../addPassword.html?${keyList.keys[element.dataset.index]}`) });
+  const passwordDataList = [];
+  let elementId;
+  let keys = [];
+  let asd = JSON.parse(localStorage.getItem(globalKey));
+  console.log(asd);
+  keys.push(JSON.parse(localStorage.getItem(globalKey)));
+  if (keys !== null) {
+    keys.forEach((element) => {
+      loginInformation = JSON.parse(localStorage.getItem(element));
+      console.log(element);
+      if (loginInformation !== null) {
+        const passwordData = {
+          WEBSITE: loginInformation.siteLink,
+          EMAIL: loginInformation.username,
+        };
+        passwordDataList.push(passwordData);
+        elementId += 1;
+      } else {
+        console.warn('no password data');
+      }
     });
+  }
+
+  const addButton = document.querySelector('.addItem');
+  addButton.addEventListener('click', () => {
+    chrome.tabs.create({ url: chrome.runtime.getURL('./addPassword.html') });
   });
-};
+
+  window.onload = () => {
+    let itemId = 0;
+
+    passwordDataList.forEach((element) => {
+      myTemplateClone = template.content.cloneNode(true);
+
+      const nameId = myTemplateClone.getElementById('WebsiteNameId');
+      const emailId = myTemplateClone.getElementById('websiteEmailId');
+      const listItem = myTemplateClone.getElementById('listItem');
+
+      listItem.dataset.index = itemId.toString();
+      itemId += 1;
+      nameId.innerHTML = element.WEBSITE;
+      emailId.innerHTML = element.EMAIL;
+
+      myUl.appendChild(myTemplateClone);
+    });
+
+    const list = document.querySelectorAll('#listItem');
+    list.forEach((element) => {
+      const dropdown = element.querySelector('#dropdown');
+      const dropdownList = element.querySelector('#dropdown-list');
+      const itemContainer = element.querySelector('#itemContainer');
+      const editButton = element.querySelector('#editButton');
+
+      console.log(element);
+      itemContainer.addEventListener(
+        'mouseover',
+        () => {
+          dropdown.style.display = 'inline-flex';
+        },
+        false,
+      );
+
+      itemContainer.addEventListener(
+        'mouseleave',
+        () => {
+          if (dropdownState === false) {
+            dropdown.style.display = 'none';
+          }
+        },
+        false,
+      );
+
+      dropdown.addEventListener(
+        'mousedown',
+        () => {
+          dropdownState ? dropdownList.style.display = 'none' : dropdownList.style.display = 'block';
+          dropdownState = !dropdownState;
+        },
+        false,
+      );
+      console.log('first');
+      editButton.addEventListener('click', () => {
+        console.log('second');
+        window.location.href = `../addPassword.html?${keys[element.dataset.index]}`;
+        chrome.tabs.create({ url: chrome.runtime.getURL(`../addPassword.html?${keys[element.dataset.index]}`) });
+      });
+    });
+  };
+}());
