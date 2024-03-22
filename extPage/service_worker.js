067396e95f720e8ff83af20f7ce89e82e2b5f78f
Hurk28 <johanneswik75@gmail.com>
2024-03-22 10:11:28 +0100

    can now read and create text files

---
+++ extPage/service_worker.js
@@ -0,0 +1,12 @@
+//importScripts("FileSaver.js");
+
+var reallyLongString = GenerateCryptKey();
+console.log(reallyLongString);
+const url = `data:text/plain,${reallyLongString}`; 
+const filename = "cryptKey.txt";
+//chrome.downloads.download({url, filename});
+
+async function GenerateCryptKey()
+{
+  return await crypto.subtle.generateKey();
+}
